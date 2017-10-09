edc02c20f67c0f563014c8c6c6938c66e6f9da05
Alvaro <ahc_frisdal@hotmail.com>
2017-10-09 16:48:54 +0200

    Ens conectem a una URL i mostrem un text amb la resposta

---
+++ app/src/main/java/edu/upc/eseiaat/pma/webconnect/WebFetcher.java
@@ -0,0 +1,41 @@
+package edu.upc.eseiaat.pma.webconnect;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by Alvaro on 09/10/2017.
+ */
+
+class WebFetcher {
+
+    static String getURL(String s_url){
+        try {
+            URL url = new URL(s_url);
+            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+            if (conn.getResponseCode()!=HttpURLConnection.HTTP_OK) {
+                return "<error: no HTTP_OK";
+            }
+            InputStream in = conn.getInputStream();
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            copyStream(in, out);
+            return out.toString();
+        } catch (IOException e) {
+            return "<error: IOException>";
+        }
+    }
+
+    private static void copyStream(InputStream in, ByteArrayOutputStream out)
+            throws IOException
+    {
+        byte[] bytes = new byte[1024];
+        int nbytes = in.read(bytes);
+        while (nbytes > 0) {
+            out.write(bytes, 0, nbytes);
+            nbytes = in.read(bytes);
+        }
+    }
+}
